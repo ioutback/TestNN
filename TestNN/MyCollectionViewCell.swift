be865617bd9d109edf55f8a0bf070e08a1ad2bb4
WengChihao <wengchihao@wengchihaos-macbook-pro.local>
2017-12-14 01:47:12 +0800

    test2

---
+++ TestNN/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  TestNN
+//
+//  Created by WengChihao on 06/10/2017.
+//  Copyright © 2017 WengChihao. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+}
--- TestNN/ViewController.swift
+++ TestNN/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDataSource , UICollectionViewDelegate{
+    
+    let imgArray = ["1" ,"2" ,"3" ,"4" , "5" , "6" ,"7" ,"8" , "9" , "10"  ]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
+        
+        cell.img.image = UIImage(named:imgArray[indexPath.row])
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
